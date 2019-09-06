06446a674cf42a5548f554c2182c4bf7b52da76c
Nikola Misic <mishke.deejay@gmail.com>
2019-09-06 10:58:23 +0200

    Wrapped the root component with BrwoserRouter

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter } from 'react-router-dom';
+
+const app = (
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
