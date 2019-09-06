4395acc7ae7eeffd1280fd95fbce1ff90aac979f
Nikola Misic <mishke.deejay@gmail.com>
2019-09-06 13:04:52 +0200

    Added example routes and page components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
 
+const HatsPage = () => (
+  <div>Hats Page</div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path='/shop/hats' component={HatsPage} />
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
