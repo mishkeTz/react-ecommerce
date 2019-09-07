1b2a409cfd95e687137922a60f121a46ff2a44dd
Nikola Misic <mishke.deejay@gmail.com>
2019-09-07 16:45:22 +0200

    Added sign in and sign up pages

--- src/App.js
+++ src/App.js
@@ -5,19 +5,44 @@ import Header from './components/header/header.component';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-and-sign-up-page/sign-and-sign-up-page.component';
+
+import { auth } from './firebase/firebase.utils';
 
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  
+  unsubscribeFromAuth = null;
+
+  state = {
+    currentUser: null,
+  }
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log('user', user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/sign-and-sign-up-page/sign-and-sign-up-page.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-and-sign-up-page.styles.scss';
+
+const SignInAndSignOutPage = () => (
+    <div className='sign-and-sign-up'>
+        <SignIn />
+    </div>
+);
+
+export default SignInAndSignOutPage;
