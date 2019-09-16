67687bf7ce1cfa19fc681139917209ca2fecb8f1
Nikola Misic <mishke.deejay@gmail.com>
2019-09-16 11:10:42 +0200

    Created shop selectors and updated collection item component and added collection page

---
+++ src/components/collections-overview/collections-overview.component.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import './collections-overview.styles.scss';
+
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+const CollectionsOverview = ({ collections }) => (
+    <div className='collections-overview'>
+        {collections.map(({ id, ...restProps}) => (
+            <CollectionPreview key={id} {...restProps} /> 
+        ))}
+    </div>
+)
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollectionsForPreview,
+})
+
+export default connect(mapStateToProps)(CollectionsOverview);
+
